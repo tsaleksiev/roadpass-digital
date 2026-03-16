eddcdb67c33beb34b2988b702dae776f43321b63
Tsvetan Aleksiev <ts.aleksiev@gmail.com>
2026-03-16 14:36:24 +0200

    fix lint errors

--- app/blueprints/trip_blueprint.rb
+++ app/blueprints/trip_blueprint.rb
@@ -9,7 +9,7 @@ class TripBlueprint < Blueprinter::Base
         field :created_at do |trip|
             trip.created_at.iso8601
           end
-      
+
           field :updated_at do |trip|
             trip.updated_at.iso8601
           end
